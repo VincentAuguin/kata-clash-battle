ec6e4199a172cf50b20fc10d9ef273bb6c798da5
Vincent Auguin <vauguin@ippon.fr>
2020-05-27 23:41:09 +0200

    feature : Create an elixirs property

--- src/main/kotlin/fr/vauguin/clashbattle/Game.kt
+++ src/main/kotlin/fr/vauguin/clashbattle/Game.kt
@@ -1,3 +1,14 @@
 package fr.vauguin.clashbattle
 
-class Game
+class Game {
+    var elixirs = INITIAL_ELIXIRS
+        set(value) {
+            field = value.coerceAtLeast(MINIMUM_ELIXIRS).coerceAtMost(MAXIMUM_ELIXIRS)
+        }
+
+    companion object {
+        private const val MINIMUM_ELIXIRS = 0
+        private const val MAXIMUM_ELIXIRS = 10
+        private const val INITIAL_ELIXIRS = 5
+    }
+}
--- src/test/kotlin/fr/vauguin/clashbattle/GameTest.kt
+++ src/test/kotlin/fr/vauguin/clashbattle/GameTest.kt
@@ -1,7 +1,9 @@
 package fr.vauguin.clashbattle
 
 import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(MockKExtension::class)
@@ -11,5 +13,26 @@ class GameTest {
     @BeforeEach
     fun setUp() {
         game = Game()
+    }
+
+    @Test
+    fun `Game should provide 5 elixirs at the beginning`() {
+        assertEquals(5, game.elixirs)
+    }
+
+    @Test
+    fun `Game should coerce at least 0 elixir`() {
+        game.elixirs = -1
+        assertEquals(0, game.elixirs)
+        game.elixirs = Int.MIN_VALUE
+        assertEquals(0, game.elixirs)
+    }
+
+    @Test
+    fun `Game should coerce at most 10 elixirs`() {
+        game.elixirs = 11
+        assertEquals(10, game.elixirs)
+        game.elixirs = Int.MAX_VALUE
+        assertEquals(10, game.elixirs)
     }
 }
