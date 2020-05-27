323ea730443bf579999892e82fd493e7df2b166e
Vincent Auguin <vauguin@ippon.fr>
2020-05-28 00:40:49 +0200

    feature : Increment elixirs for each passed second

--- src/main/kotlin/fr/vauguin/clashbattle/Game.kt
+++ src/main/kotlin/fr/vauguin/clashbattle/Game.kt
@@ -1,14 +1,50 @@
 package fr.vauguin.clashbattle
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ProducerScope
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlin.coroutines.coroutineContext
+
+@ExperimentalCoroutinesApi
 class Game {
     var elixirs = INITIAL_ELIXIRS
         set(value) {
             field = value.coerceAtLeast(MINIMUM_ELIXIRS).coerceAtMost(MAXIMUM_ELIXIRS)
         }
 
+    private var elixirsProduction: ReceiveChannel<Int>? = null
+
+    suspend fun startProducingElixirs() {
+        if (elixirsProduction != null)
+            return
+
+        CoroutineScope(coroutineContext).run {
+            produce<Int> { produceElixirs() }.also { channel ->
+                elixirsProduction = channel
+                storeElixirs(channel)
+            }
+        }
+    }
+
+    private suspend fun ProducerScope<Int>.produceElixirs() {
+        while (isActive) {
+            delay(INITIAL_ELIXIRS_CYCLE_MILLIS)
+            send(ELIXIRS_PRODUCED_PER_CYCLE)
+        }
+        close()
+    }
+
+    private suspend fun CoroutineScope.storeElixirs(channel: ReceiveChannel<Int>) = launch {
+        for (quantity in channel)
+            elixirs += quantity
+    }
+
     companion object {
         private const val MINIMUM_ELIXIRS = 0
         private const val MAXIMUM_ELIXIRS = 10
         private const val INITIAL_ELIXIRS = 5
+        private const val INITIAL_ELIXIRS_CYCLE_MILLIS = 1_000L
+        private const val ELIXIRS_PRODUCED_PER_CYCLE = 1
     }
 }
--- src/test/kotlin/fr/vauguin/clashbattle/GameTest.kt
+++ src/test/kotlin/fr/vauguin/clashbattle/GameTest.kt
@@ -1,11 +1,16 @@
 package fr.vauguin.clashbattle
 
 import io.mockk.junit5.MockKExtension
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
+@ExperimentalCoroutinesApi
 @ExtendWith(MockKExtension::class)
 class GameTest {
     lateinit var game: Game
@@ -34,5 +39,34 @@ class GameTest {
         assertEquals(10, game.elixirs)
         game.elixirs = Int.MAX_VALUE
         assertEquals(10, game.elixirs)
+    }
+
+    @Test
+    fun `Game should increment elixirs for each passed seconds`() = runBlockingTest {
+        game.elixirs = 0
+        val production = launch { game.startProducingElixirs() }
+        advanceTimeBy(2_000)
+        production.cancel("End of test")
+        assertEquals(2, game.elixirs)
+    }
+
+    @Test
+    fun `Game should not increment elixirs if a second is not completely passed`() = runBlockingTest {
+        game.elixirs = 0
+        val production = launch { game.startProducingElixirs() }
+        advanceTimeBy(999)
+        production.cancel("End of test")
+        assertEquals(0, game.elixirs)
+    }
+
+    @Test
+    fun `Game should not have multiple elixirs production in parallel`() = runBlockingTest {
+        game.elixirs = 0
+        val production1 = launch { game.startProducingElixirs() }
+        val production2 = launch { game.startProducingElixirs() }
+        advanceTimeBy(1_000)
+        production1.cancel("End of test")
+        production2.cancel("End of test")
+        assertEquals(1, game.elixirs)
     }
 }
